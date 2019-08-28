c838c3b0c12a08b38c62c51250afdf18e8b34cc3
Victor Arvidsson <victarv@gmail.com>
2019-08-28 14:01:20 +0200

    Started 1.6 challenge

--- 1.6 Hur gammal är du.py
+++ 1.6 Hur gammal är du.py
@@ -1,6 +1,16 @@
+import time
+
 year = input('What year were you born?')
 month = input('What month?')
 day = input('What day?')
 
-print('OK, then you will turn {} on {} {} in 2019!'.format(
-    2019-int(year), month, day))
+age = time.strptime('{} {} {}'.format(year, month, day), '%Y %m %d')
+
+age_seconds = time.mktime(age)
+now_seconds = time.time()
+
+time_diff = now_seconds - age_seconds
+
+print('You are {} seconds old'.format(int(time_diff)))
+# print('OK, then you will turn {} on {} {} in 2019!'.format(
+#     2019-int(year), month, day))
