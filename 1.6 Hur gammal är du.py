ab3b7d69dd03ec7461d8a96395fc38df12060ad2
Victor Arvidsson <victarv@gmail.com>
2019-08-28 12:52:41 +0200

    Create 1.6 Hur gammal är du.py

---
+++ 1.6 Hur gammal är du.py
@@ -0,0 +1,5 @@
+year = input('What year were you born?')
+month = input('What month?')
+day = input('What day?')
+
+print('OK, then you will turn {} on {} {} in 2019!'.format(2019-int(year), month, day))
