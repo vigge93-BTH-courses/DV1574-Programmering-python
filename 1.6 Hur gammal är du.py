1b7c54b93ff937a731a48fb71ffe4d7fb29f10c8
Victor Arvidsson <victarv@gmail.com>
2019-08-28 13:17:10 +0200

    Formatting

--- 1.6 Hur gammal är du.py
+++ 1.6 Hur gammal är du.py
@@ -2,4 +2,5 @@ year = input('What year were you born?')
 month = input('What month?')
 day = input('What day?')
 
-print('OK, then you will turn {} on {} {} in 2019!'.format(2019-int(year), month, day))
+print('OK, then you will turn {} on {} {} in 2019!'.format(
+    2019-int(year), month, day))
--- Simple plot.py
+++ Simple plot.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as pl
 import numpy
 
-x = numpy.linspace(0, 20, 100) # Creates an array with 100 elements evenly distibuted between 0 and 20
-pl.plot(x, numpy.sin(x)) # x, f(x)
+# Creates an array with 100 elements evenly distibuted between 0 and 20
+x = numpy.linspace(0, 20, 100)
+
+pl.plot(x, numpy.sin(x))  # x, f(x)
 pl.show()
