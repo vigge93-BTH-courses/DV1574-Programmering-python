77c32a972b28b97d8391fe4a5ee02ed7beb6250e
Victor <victarv@gmail.com>
2019-09-02 21:25:10 +0200

    Create 1.29 Skriv om större if-sats.py

---
+++ 1.29 Skriv om större if-sats.py
@@ -0,0 +1,34 @@
+def original(n):
+    if n > 10:
+        if n > 100:
+            if n < 1000:
+                print(n)
+            else:
+                if n < 1001:
+                    print('Pretty large')
+                else:
+                    print(n)
+        else:
+            if n == 11:
+                print(n)
+            else:
+                if n == 10:
+                    print('10!')
+                else:
+                    print(n)
+    else:
+        if n < 0:
+            print('Below zero')
+        else:
+            print(n)
+
+
+def new(n):
+    if n < 0:
+        print('Below zero')
+    elif n == 10:
+        print('10!')
+    elif n == 1000:
+        print('Pretty large')
+    else:
+        print(n)
