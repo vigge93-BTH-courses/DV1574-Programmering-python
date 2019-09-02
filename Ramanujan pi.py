363cd6dac2c64f7025f60dabd1b48d2ac90c229a
Victor <victarv@gmail.com>
2019-09-02 22:22:46 +0200

    Create Ramanujan pi.py

---
+++ Ramanujan pi.py
@@ -0,0 +1,16 @@
+import math
+
+
+def pi():
+    sum = 0
+    val = 1
+    k = 0
+    while val > 10**(-15):
+        val = (math.factorial(4 * k) * (1103 + 25390 * k)) / \
+            (math.factorial(k) ** 4 * 396 ** (4 * k))
+        sum += val
+        k += 1
+    return 9801/(sum * 2 * math.sqrt(2))
+
+
+print(pi())
